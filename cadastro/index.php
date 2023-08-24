22ab3e9502dcbbc404d39eaed07443970e9d33d2
Kayk Santos <80800368+KaykSantos@users.noreply.github.com>
2023-08-24 20:31:20 -0300

    Add files via upload

--- cadastro/index.php
+++ cadastro/index.php
@@ -5,20 +5,118 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <style>
+        *{
+            margin: 0px;
+            padding: 0px;
+            font-family: Arial;
+        }
+
+        header{
+            height: 70px;
+            width: 100vw;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+            background-color: black;
+        }
+
+        header p{
+            color: white;
+            font-size: 25px;
+            letter-spacing: 2px;
+            margin: 0px 15px;
+            font-weight: bolder;
+        }
+
+        header a{
+            text-decoration: none;
+            color: white;
+            font-size: 25px;
+            letter-spacing: 2px;
+            margin: 0px 15px;
+            font-weight: bolder;
+        }
+
+        header a:hover{
+            text-decoration: underline;
+        }
+
+        main{
+            width: 100vw;
+            height: calc(100vh - 70px);
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: rgb(240, 240, 240);
+        }
+
+        form{
+            background-color: white;
+            width: 300px;
+            height: auto;
+            padding: 20px 20px;
+            border-radius: 5px;
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+            -webkit-box-shadow: 0px 0px 26px -8px rgba(0,0,0,0.75);
+            -moz-box-shadow: 0px 0px 26px -8px rgba(0,0,0,0.75);
+            box-shadow: 0px 0px 26px -8px rgba(0,0,0,0.75);
+        }
+
+        form p{
+            font-size: 25px;
+            margin: 0px 15px;
+            font-weight: bolder;
+        }
+
+        input{
+            margin: 15px 0px;
+            outline: none;
+            border: none;
+            border-bottom: 1px solid black;
+            padding: 4px;
+            font-size: 16px;
+        }
+
+        button{
+            width: 100px;
+            height: 30px;
+            margin: auto auto;
+        }
+
+        button{
+            cursor: pointer;
+            outline: none;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            font-weight: bolder;
+            letter-spacing: 1px;
+            background-color: black;
+            color: white;
+        }
+    </style>
 </head>
 <body>
-    <a href="../"><h2>Voltar</h2></a>
-    <fieldset>
-        <legend>Cadastro</legend>
+    <header>
+        <p>Cadastros</p>
+        <a href="../">Voltar</a>
+    </header>
+    <main>
         <form action="../php/cadastrar.php" method="post" name="frmcadastro">
-            <input type="text" name="name" id="name" placeholder="Digite seu nome:">
-            <br><br>
-            <input type="email" name="email" id="email" placeholder="Digite seu email:">
-            <br><br>
-            <input type="password" name="password" id="password" placeholder="Digite sua senha:">
-            <br><br>
+            <p>Cadastrar cliente</p>
+            <input type="text" name="name" id="name" placeholder="Digite o nome:">
+            
+            <input type="email" name="email" id="email" placeholder="Digite o email:">
+            
+            <input type="password" name="password" id="password" placeholder="Digite a senha:">
+            
             <button name="submit">Cadastrar</button>
-        </form>
-    </fieldset>
+        </form>        
+    </main>
 </body>
 </html>
--- relatorio/index.php
+++ relatorio/index.php
@@ -8,6 +8,88 @@ include('../php/conect.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Relatório</title>
     <style>
+        *{
+            margin: 0px;
+            padding: 0px;
+            font-family: Arial;
+        }
+
+        header{
+            height: 70px;
+            width: 100vw;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: center;
+            background-color: black;
+        }
+
+        header p{
+            color: white;
+            font-size: 25px;
+            letter-spacing: 2px;
+            margin: 0px 15px;
+            font-weight: bolder;
+        }
+
+        header a{
+            text-decoration: none;
+            color: white;
+            font-size: 25px;
+            letter-spacing: 2px;
+            margin: 0px 15px;
+            font-weight: bolder;
+        }
+
+        header a:hover{
+            text-decoration: underline;
+        }
+
+        main{
+            width: 100vw;
+            height: calc(100vh - 70px);
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: rgb(240, 240, 240);
+        }
+
+        .container{
+            background-color: white;
+            width: 550px;
+            height: auto;
+            padding: 20px 20px;
+            border-radius: 5px;
+            display: flex;
+            flex-direction: column;
+            text-align: center;
+            -webkit-box-shadow: 0px 0px 26px -8px rgba(0,0,0,0.75);
+            -moz-box-shadow: 0px 0px 26px -8px rgba(0,0,0,0.75);
+            box-shadow: 0px 0px 26px -8px rgba(0,0,0,0.75);
+        }
+
+        input{
+            margin: 15px 0px;
+            outline: none;
+            border: none;
+            border-bottom: 1px solid black;
+            padding: 4px;
+            font-size: 16px;
+        }
+
+        button{
+            cursor: pointer;
+            outline: none;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            font-weight: bolder;
+            letter-spacing: 1px;
+            background-color: black;
+            color: white;
+        }
+
         p{
             font-size: 30px;
             font-weight: bolder;
@@ -17,6 +99,7 @@ include('../php/conect.php');
         td{
             border: 1px solid black;
             text-align: center;
+            padding: 3px;
         }
         #tb-id{
             width:40px;
@@ -28,11 +111,14 @@ include('../php/conect.php');
             width:200px;
         }
         #tb-ex{
-            width:50px;
+            width:70px;
+        }
+        #tb-ed{
+            width: 70px;
         }
         a{
             text-decoration: none;
-            color: red;
+            color: black;
         }
         a:hover{
             text-decoration: underline;
@@ -43,40 +129,49 @@ include('../php/conect.php');
     </style>
 </head>
 <body>
-    <a href="../"><h2>Voltar</h2></a>
-    <p>Relatório</p>
-    <?php
-        $sql = "SELECT * FROM users";
-        $stmt = $pdo->prepare($sql);
-        if($stmt->execute()){
-            if($stmt->rowcount() == 0){
-                echo "<h2>Nenhum usuário cadastrado!</h2>";
-            }else{
-                echo "
-                    <table>
-                        <tr>
-                            <td id='tb-id'>ID</td>
-                            <td id='tb-nm'>Nome</td>
-                            <td id='tb-em'>Email</td>
-                            <td id='tb-ex'>Excluir</td>
-                            <td id='tb-ed'>Editar</td>
-                        </tr>
-                ";
-                foreach($stmt as $row){
+    <header>    
+        <p>Relatório</p>
+        <a href="../">Voltar</a>
+    </header>
+    <main>
+        <?php
+            $sql = "SELECT * FROM users";
+            $stmt = $pdo->prepare($sql);
+            if($stmt->execute()){
+                if($stmt->rowcount() == 0){
+                    echo "<h2>Nenhum usuário cadastrado!</h2>";
+                }else{
                     echo "
-                        <tr>
-                            <td>".$row['cd']."</td>
-                            <td>".$row['name']."</td>
-                            <td>".$row['email']."</td>
-                            <td><a href='?id=".$row['cd']."&type=delete'>Delete</a></td>
-                            <td><a href='?id=".$row['cd']."&type=edit'>Editar</a></td>
-                        </tr>
+                    <div class='container'>
+                        <h3>Usuários cadastrados:</h3>
+                        <table>
+                            <tr>
+                                <td id='tb-id'>ID</td>
+                                <td id='tb-nm'>Nome</td>
+                                <td id='tb-em'>Email</td>
+                                <td id='tb-ex'>Excluir</td>
+                                <td id='tb-ed'>Editar</td>
+                            </tr>
                     ";
+                    foreach($stmt as $row){
+                        echo "
+                            
+                                <tr>
+                                    <td>".$row['cd']."</td>
+                                    <td>".$row['name']."</td>
+                                    <td>".$row['email']."</td>
+                                    <td><a href='?id=".$row['cd']."&type=delete'>Delete</a></td>
+                                    <td><a href='?id=".$row['cd']."&type=edit'>Editar</a></td>
+                                </tr>
+                            
+                        ";
+                    }
+                    echo "</table></div>";
                 }
-                echo "</table>";
             }
-        }
-    ?>
+        ?>
+    </main>
+    
 </body>
 </html>
 <?php
